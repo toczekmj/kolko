d714a1d47fdd7ef0dec67efab8bf873a46d21cd3
mikronik24 <mikronik24@gmail.com>
2018-11-02 19:46:25 +0100

    Update sza.cpp

--- Kółko 5/sza.cpp
+++ Kółko 5/sza.cpp
@@ -5,25 +5,19 @@ int main()
 {
     ios_base::sync_with_stdio(false);
     int n; cin>>n;
-
     int tab[n][n];
 
-    for(int i = 0; i < n; i++){
-
-        for(int j = 0; j < n; j++){
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < n; j++)
             cin>>tab[i][j];
-        }
-
-    }
 
     int suma = 0;
 
     for(int i = 0; i < n; i++){
         int maks = 0;
-        for(int j = 0; j < n; j++){
+        for(int j = 0; j < n; j++)
             if(tab[i][j] > maks)
                 maks = tab[i][j];
-        }
         suma += maks;
     }
     cout<<suma;
