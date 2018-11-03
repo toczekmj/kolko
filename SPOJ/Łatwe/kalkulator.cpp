ab981cda8f2a7fc82fa062e521445cdc8907657b
mikronik24 <mikronik24@gmail.com>
2018-11-03 23:18:01 +0100

    cm

--- SPOJ/Łatwe/kalkulator.cpp
+++ SPOJ/Łatwe/kalkulator.cpp
@@ -5,26 +5,25 @@ using namespace std;
 
 const int MAX = 1000005;
 int tab[MAX];
+int b,c,i = 0;
 
-int main()
+
+void read()
 {
   string a;
-  int b,c;
-  int i = 0;
   string plus = "+";
   string minus = "-";
-  string div = "/";
+  string divd = "/";
   string mult = "*";
   string mod = "%";
-
-
+  
   while(cin>>a>>b>>c)
   {
     if(a == plus)
       tab[i] = b+c;
     else if(a == minus)
       tab[i] = b-c;
-    else if(a == div)
+    else if(a == divd)
       tab[i] = b/c;
     else if(a == mult)
       tab[i] = b*c;
@@ -32,10 +31,18 @@ int main()
       tab[i] = b%c;
     i++;
   }
+}
 
+void show()
+{
   for(int j = 0; j < i; j++)
   {
     cout<<tab[j]<<endl;
   }
+}
 
+int main()
+{
+  read();
+  show();
 }
