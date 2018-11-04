4f68d7c1283e2b2ef000067c61141f4166fd43df
mikronik24 <mikronik24@gmail.com>
2018-11-04 21:29:55 +0100

    f

---
+++ codewars/transform_to_prime.cpp
@@ -0,0 +1,32 @@
+#include "iostream"
+
+using namespace std;
+int i;
+
+bool isPrime(int t)
+{
+  if(t > 1)
+  {
+    for(i = 2; i < t; i++)
+    {
+      if (t%i == 0) {
+        return false;
+      }
+    }
+    if(t == i)
+    {
+      return true;
+    }
+  }else return false;
+}
+
+int main()
+{
+  int t;
+  cin>>t;
+  if(isPrime(t)){
+    cout<<"tak";
+  }else cout<<"nie";
+
+
+}
