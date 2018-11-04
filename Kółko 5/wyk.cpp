6aafb23b6edbb52ea9739df089a29459722ff5c9
mikronik24 <mikronik24@gmail.com>
2018-11-04 21:45:14 +0100

    f

--- Kółko 5/wyk.cpp
+++ Kółko 5/wyk.cpp
@@ -5,11 +5,9 @@ int main()
     int n; cin>>n;
     int start = 0, ile = 0;
     for(int i = 0; i < n; i++){
-        int k;
-        cin>>k;
-        if(k == start+1){
-            start++;
-        }else ile++;
+        int k; cin>>k;
+        if(k == start+1) start++; 
+        else ile++;
     }
     cout<<ile;
 }
